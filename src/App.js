0d3a3ee247d1ea339c276a43babcd7673de284e0
Balthasar Hofer <lebalz@outlook.com>
2020-04-26 12:16:42 +0200

    add sensor simulator (for devices without accelerometer)

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import "./styles.css";
 import socketioClient from "socket.io-client";
 import LineGraph from "./components/LineGraph";
 import SensorConfig from "./components/SensorConfig";
+import MotionSimulator from "./Simulator";
 
 export const API_URL = "http://localhost:4001";
 
 export default class App extends React.Component {
+  deviceName = Math.random().toString(36).substr(2, 5).toUpperCase();
+
   state = {
     motionData: [],
-    deviceName: Math.random().toString(36).substr(2, 5),
     displayDevice: '',
-    devices: []
+    devices: [],
+    useSimulatedDeviceMotion: false
   };
 
   constructor() {
@@ -20,29 +23,50 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
+    this.deviceSimulator = document.getElementById('DeviceSimulator');
+
+    /**
+     * configure how to treat to new messages from the server 
+     */
     this.socket.on("motion_devices", data => this.setState({ devices: data }));
     this.socket.on("motion_data", data => {
       if (data) {
         this.setState({ motionData: data })
       }
     });
+
     /**
      * register my device
      */
-    this.socket.emit('new_device', { name: this.state.deviceName });
-    // join the datastream of my device
-    this.setDisplayDevice(this.state.deviceName);
+    this.socket.emit('new_device', { name: this.deviceName });
+    /**
+     * ensure to receive the latest version of motion devices
+     */
+    this.socket.emit('get_devices');
 
     /**
-     * register the browser as a device motion listener
-     * this.onDevicemotion is called on every new sensor value
+     * join the datastream of my device
      */
-    window.ondevicemotion = this.onDevicemotion;
+    this.setDisplayDevice(this.deviceName);
+
+    /**
+     * register a function that is called whenever the window emits a
+     * device motion event (e.g. whenever a new sensorvalue is present)
+     */
+    window.addEventListener('devicemotion', this.onDevicemotion, true)
+  }
+
+  componentWillUnmount() {
+    /**
+     * cleanup - remove the eventlistener
+     */
+    window.removeEventListener('devicemotion', this.onDevicemotion, true);
+    this.socket.emit("remove_device", { name: this.deviceName });
   }
 
   onDevicemotion = (e) => {
     const motionData = {
-      name: this.state.deviceName,
+      name: this.deviceName,
       timeStamp: e.timeStamp,
       x: e.accelerationIncludingGravity.x,
       y: e.accelerationIncludingGravity.y,
@@ -51,9 +75,42 @@ export default class App extends React.Component {
     this.socket.emit("new_motion_data", motionData);
   };
 
+  toggleUseSimulatedDeviceMotion = () => {
+    const deviceSimulator = document.getElementById('DeviceSimulator');
+    if (!deviceSimulator) {
+      return;
+    }
+
+    const useSimulator = !this.state.useSimulatedDeviceMotion;
+    this.setState({ useSimulatedDeviceMotion: useSimulator });
+    if (useSimulator) {
+      window.removeEventListener('devicemotion', this.onDevicemotion, true);
+      deviceSimulator.addEventListener('devicemotion', this.onDevicemotion, true);
+      const simulator = new MotionSimulator();
+      this.setState({
+        simulator: simulator
+      });
+    } else {
+      deviceSimulator.removeEventListener('devicemotion', this.onDevicemotion, true);
+      window.addEventListener('devicemotion', this.onDevicemotion, true);
+      if (this.state.simulator) {
+        this.state.simulator.stopSimulation();
+        this.setState({
+          simulator: undefined
+        });
+      }
+    }
+  }
+
   setDisplayDevice = (deviceName) => {
     // subscribe to the new sensor data
-    this.socket.emit('display_device', { name: deviceName, oldDevice: this.state.displayDevice });
+    this.socket.emit(
+      'display_device',
+      {
+        name: deviceName,
+        oldDevice: this.state.displayDevice
+      }
+    );
     // save the displayDevice locally
     this.setState({ displayDevice: deviceName });
   }
@@ -62,16 +119,30 @@ export default class App extends React.Component {
     this.socket.emit('clear_motion_data', { name: this.state.displayDevice })
   }
 
+  clearDevices = () => {
+    const response = window.confirm("Do you really want to clear all devices?");
+    if (response === true) {
+      this.socket.emit('clear_devices');
+      this.socket.emit('new_device', { name: this.deviceName });
+      this.socket.emit('get_devices');
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Socket Sensor Stream</h1>
-        <button onClick={this.clearMotionData}>Clear</button>
+        <span>
+          <button onClick={this.clearMotionData}>Clear current Motion Data</button>
+          <button onClick={this.clearDevices}>Clear Devices</button>
+        </span>
         <SensorConfig
           setDisplayDevice={this.setDisplayDevice}
           devices={this.state.devices}
-          deviceName={this.state.deviceName}
+          deviceName={this.deviceName}
           displayDevice={this.state.displayDevice}
+          toggleUseSimulatedDeviceMotion={this.toggleUseSimulatedDeviceMotion}
+          useSimulatedDeviceMotion={this.state.useSimulatedDeviceMotion}
         />
         <LineGraph motionData={this.state.motionData} />
       </div>
--- src/components/SensorConfig.jsx
+++ src/components/SensorConfig.jsx
@@ -10,27 +10,38 @@ export default class SensorConfig extends React.Component {
   render() {
     return (
       <div className="sensor-config">
-        <div>Device Name: <b>{this.props.deviceName}</b></div>
+        <div>My Device Name: <b>{this.props.deviceName}</b></div>
 
-        <label htmlFor="devices">Choose a display device:</label>
-        <select
-          id="devices"
-          onChange={(e, device) =>  this.props.setDisplayDevice(e.target.value)}
-          value={this.props.displayDevice}
-        >
-          {
-            this.props.devices.map(device => {
-              return (
-                <option
-                  key={device}
-                  value={device}
-                >
-                  {device}
-                </option>
-              )
-            })
-          }
-        </select>
+        <span>
+          <label htmlFor="devices">Choose a display device: </label>
+          <select
+            id="devices"
+            onChange={(e, device) => this.props.setDisplayDevice(e.target.value)}
+            value={this.props.displayDevice}
+          >
+            {
+              this.props.devices.map(device => {
+                return (
+                  <option
+                    key={device}
+                    value={device}
+                  >
+                    {device}
+                  </option>
+                )
+              })
+            }
+          </select>
+        </span>
+        <span>
+          <label htmlFor="use-simlated-devicemotion">Use simulated Device Motion</label>
+          <input
+            id="use-simlated-devicemotion"
+            type="checkbox"
+            onChange={this.props.toggleUseSimulatedDeviceMotion}
+            checked={this.props.useSimulatedDeviceMotion}
+          />
+        </span>
       </div>
     );
   }
